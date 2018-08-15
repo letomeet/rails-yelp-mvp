08b6e1eebbc0c46092c838f891d483b56a6d2803
loig kerebel <kerebel.loig@gmail.com>
2018-08-15 16:25:03 +0200

    reste mise en forme a faire

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+def random_restaurant
+  category = ["chinese", "italian", "japanese", "french", "belgian"]
+  category.sample
+end
+
+
+5.times do
+  restaurant = Restaurant.new(
+  name: Faker::Pokemon.name,
+  address: Faker::Space.planet,
+  phone_number: Faker::PhoneNumber.phone_number,
+  category: random_restaurant
+  )
+  restaurant.save!
+end
